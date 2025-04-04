db95f65091d4590ce62ec6b39849c2dd56c301df
ayush mishra <ayushmishra9799@gmail.com>
2025-04-04 13:23:05 +0530

    1st

---
+++ horizon_comp/src/App.jsx
@@ -0,0 +1,95 @@
+// import { useState } from "react";
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <div
+          style={{
+            width: "333px",
+            height: "283px",
+            backgroundColor: "#D9D9D9",
+            marginTop: "-249px",
+            marginLeft: "-221px",
+            border: "1px solid black",
+            position: "relative",
+          }}
+        >
+          <div
+            style={{
+              width: "172px",
+              height: "129px",
+              backgroundColor: "#DA3333",
+              marginTop: "-175px",
+              marginLeft: "-139px",
+              position: "absolute",
+              border: "1px solid black",
+              top: "250px",
+              left: "220px",
+            }}
+          >
+            <div
+              style={{
+                width: "106px",
+                height: "85px",
+                backgroundColor: "#DA3333",
+                marginTop: "-224px",
+                marginLeft: "-194px",
+                position: "absolute",
+                border: "1px solid black",
+                top: "210px",
+                left: "150px",
+              }}
+            >
+              <div
+                style={{
+                  width: "106px",
+                  height: "85px",
+                  backgroundColor: "#DA3333",
+                  marginTop: "-90px",
+                  marginLeft: "-194px",
+                  position: "absolute",
+                  border: "1px solid black",
+                  top: "200px",
+                  left: "190px",
+                }}
+              >
+                <div
+                  style={{
+                    width: "75px",
+                    height: "69px",
+                    backgroundColor: "#1D1212",
+                    marginTop: "-224px",
+                    marginLeft: "-7px",
+                    position: "relative",
+                    border: "1px solid black",
+                    top: "110px",
+                    left: "200px",
+                  }}
+                >
+                  <div
+                    style={{
+                      width: "75px",
+                      height: "69px",
+                      backgroundColor: "#1D1212",
+                      marginTop: "-88px",
+                      marginLeft: "-5px",
+                      position: "relative",
+                      border: "1px solid black",
+                      top: "200px",
+                    }}
+                  ></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
